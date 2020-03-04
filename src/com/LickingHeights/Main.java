07ae61f1d8cff8d82f3e94901f0a32f1774ca0dd
Karim <karimkk05@gmail.com>
2020-03-04 13:44:15 -0500

    Intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,40 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("+----------------------------------------------+");
+        System.out.println("|           Gas station                        |");
+        System.out.println("|       THE      2LETHAL    STATION            |");
+        System.out.println("|                                              |");
+        System.out.println("|  1955 Polaris Pk,  Columbus,   OH 43240      |");
+        System.out.println("|  7820 E Main St, Reynoldsburg, OH 43068      |");
+        System.out.println("|  1225 Hill Rd N, Pickerington, OH 43147      |");
+        System.out.println("|                                              |");
+        System.out.println("|                                              |");
+        System.out.println("|   DATE                         TIME          |");
+        System.out.println("|   02/08/05                     4:48pm        |");
+        System.out.println("|                                              |");
+        System.out.println("|   INVOICE#       0101010                     |");
+        System.out.println("|   AUTH#                                      |");
+        System.out.println("|               MASTERCARD                     |");
+        System.out.println("|             ACCOUNT NUMBER                   |");
+        System.out.println("|              ************00                  |");
+        System.out.println("|                                              |");
+        System.out.println("|                                              |");
+        System.out.println("|       PUMP       PRODUCT       $/G           |");
+        System.out.println("|        01          UNLD         2.5          |");
+        System.out.println("|                                              |");
+        System.out.println("|     GALLONS      FUEL           TOTAL        |");
+        System.out.println("|     20.00         $             $50.00       |");
+        System.out.println("|                                              |");
+        System.out.println("|                                              |");
+        System.out.println("|                THANK  YOU!                   |");
+        System.out.println("|                GAS STATION                   |");
+        System.out.println("|                  BRAND                       |");
+        System.out.println("+----------------------------------------------+");
+
+    }
+        }
